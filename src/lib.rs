99b87ae67b36b36ef217ed58426b2e7a1cd5026c
jordins <jordi.nistal@gmail.com>
2019-08-24 22:45:25 +0200

    refactor(main): move logic from main to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
-pub mod prepender;
+use std::process;
 pub mod filemanager;
+pub mod prepender;
+
+pub fn append() {
+    let files_to_modify = filemanager::list_files("./test/test-input-data".to_string());
+    for file in files_to_modify {
+        let content = "This is my new content\n";
+        println!("Going to prepend content to {}", file);
+
+        if let Err(e) = prepender::prepend_content_to_file(content, &file) {
+            println!("Application error: {}", e);
+
+            process::exit(1);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-extern crate copyrightfiles;
-use std::process;
-
 fn main() {
-    let files_to_modify =
-        copyrightfiles::filemanager::list_files("./test/test-input-data".to_string());
-    for file in files_to_modify {
-        let content = "This is my new content\n";
-        println!("Going to prepend content to {}", file);
-
-        if let Err(e) = copyrightfiles::prepender::prepend_content_to_file(content, &file) {
-            println!("Application error: {}", e);
-
-            process::exit(1);
-        }
-    }
+    copyrightfiles::append();
 }
