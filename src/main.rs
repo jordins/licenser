74f3c8c85f3112cb28fb853bab4af882858dfd88
jordins <jordi.nistal@gmail.com>
2019-08-25 12:06:59 +0200

    feat(cli): add command line argument to pass the folder
    usage: licenser --folder ./test/initial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{App, Arg};
+
 fn main() {
-    licenser::prepend("This is my new content\n", "./test/initial");
+    let matches = App::new("Licenser")
+        .version("0.1.0")
+        .author("Jordi Nistal <jordi.nistal@gmail.com>")
+        .about("Adds license headers to your files")
+        .arg(
+            Arg::with_name("folder")
+                .short("f")
+                .long("folder")
+                .value_name("FOLDER")
+                .help("Specifies the folder where to add the licence"),
+        )
+        .get_matches();
+    let folder = matches.value_of("folder").unwrap();
+    println!("Value for folder: {}", folder);
+
+    licenser::prepend("This is my new content\n", folder);
 }
